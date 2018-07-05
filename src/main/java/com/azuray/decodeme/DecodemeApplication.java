891d4d6e4102a1b33c5013d6c962b54260e65f4f
yangbao <yangbao@baidu.com>
2018-07-05 10:13:05 +0800

    提交xml配置的导入和properties的导入

--- src/main/java/com/azuray/decodeme/DecodemeApplication.java
+++ src/main/java/com/azuray/decodeme/DecodemeApplication.java
@@ -3,9 +3,11 @@ package com.azuray.decodeme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.azuray.decodeme.dao"})
+@ImportResource(locations = {"classpath:/application.xml"})
 public class DecodemeApplication {
 
   public static void main(String[] args) {
