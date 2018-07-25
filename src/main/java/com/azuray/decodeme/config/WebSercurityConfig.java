3b23d5e4e78aa5bb5e111fef78a41e5a114cb361
yangbao <yangbao@baidu.com>
2018-07-25 18:43:20 +0800

    提交更新后的配置

--- src/main/java/com/azuray/decodeme/config/WebSercurityConfig.java
+++ src/main/java/com/azuray/decodeme/config/WebSercurityConfig.java
@@ -1,14 +1,20 @@
 package com.azuray.decodeme.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import com.azuray.decodeme.filter.PermitAuthenticationFilter;
+
 @Configuration
 @EnableWebSecurity
 public class WebSercurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private PermitAuthenticationFilter permitAuthenticationFilter;
+
     //
     //    @Override
     //    @Bean
@@ -18,8 +24,13 @@ public class WebSercurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/", "/user/*","*/demo/*").anonymous().
-                anyRequest().authenticated();
+        http.anonymous().and()
+                .authorizeRequests()
+                .antMatchers("/","/demo*/**").permitAll()
+             .and().
+             authorizeRequests().
+                anyRequest().authenticated().and().rememberMe();
+
         //        .and().formLogin()
         //                .loginPage("/login")
         //                .permitAll()
