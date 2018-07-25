2f33e92e0e05a1595c959aa3170dacfb76bc7aab
yangbao <yangbao@baidu.com>
2018-07-25 20:22:56 +0800

    修复错误的引用

--- src/main/java/com/azuray/decodeme/config/WebSercurityConfig.java
+++ src/main/java/com/azuray/decodeme/config/WebSercurityConfig.java
@@ -1,19 +1,14 @@
 package com.azuray.decodeme.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-import com.azuray.decodeme.filter.PermitAuthenticationFilter;
-
 @Configuration
 @EnableWebSecurity
 public class WebSercurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private PermitAuthenticationFilter permitAuthenticationFilter;
 
     //
     //    @Override
