a1a71e58c89bc3f83ae9b65ff31a8310723560b7
yangbao <yangbao@baidu.com>
2018-07-31 11:24:30 +0800

    开启事务控制

--- src/main/java/com/azuray/decodeme/DecodemeApplication.java
+++ src/main/java/com/azuray/decodeme/DecodemeApplication.java
@@ -4,10 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+// 开启扫描controller，service，mapper
 @ComponentScan(basePackages = {"com.azuray.decodeme.mapper*","com.azuray.decodeme.controller","com.azuray.decodeme.service"})
+// 开启扫描xml
 @ImportResource(locations = {"classpath:/applicationContext.xml"})
+// 开启事务
+@EnableTransactionManagement
 public class DecodemeApplication {
 
   public static void main(String[] args) {
