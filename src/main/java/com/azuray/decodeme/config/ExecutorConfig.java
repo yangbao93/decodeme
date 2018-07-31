c2c82594e7fc98c7386d4473766d1eb5b78b140b
yangbao <yangbao@baidu.com>
2018-07-31 11:20:54 +0800

    通用异步线程池操作

---
+++ src/main/java/com/azuray/decodeme/config/ExecutorConfig.java
@@ -0,0 +1,46 @@
+package com.azuray.decodeme.config;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+public class ExecutorConfig {
+
+    /**
+     * 核心线程数
+     */
+    private static final int CORE_POOL_SIZE = 5;
+    /**
+     * 最大线程数
+     */
+    private static final int MAX_POOL_SIZE =100;
+    /**
+     *
+     */
+    private static final int QUEUE_CAPACITY = 10;
+
+    /**
+     * 通用线程池前缀
+     */
+    private static final String COMMON_THREAD_POOL_PREFIX = "common-execuor-";
+
+    @Bean(value = "commonAsync")
+    public Executor commonAsync(){
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(CORE_POOL_SIZE);
+        executor.setMaxPoolSize(MAX_POOL_SIZE);
+        executor.setQueueCapacity(QUEUE_CAPACITY);
+        executor.setThreadNamePrefix(COMMON_THREAD_POOL_PREFIX);
+        // rejection-policy：当pool已经达到max size的时候，如何处理新任务
+        // CALLER_RUNS：不在新线程中执行任务，而是有调用者所在的线程来执行
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.initialize();
+        return executor;
+    }
+}
