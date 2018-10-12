81c0c6e7b6f50dde6a965dbc066db2b55410ec69
yangbao <yangbao@baidu.com>
2018-10-12 10:40:33 +0800

    1.解决idea启动时候出现sqlSession空指针的问题，如果启动报错则需要在mybatisplusconfig中配置url等信息；2.注释掉多余的mybatisplus引用

--- decodeme-service/src/main/java/com/azuray/decodeme/config/MybatisPlusConfig.java
+++ decodeme-service/src/main/java/com/azuray/decodeme/config/MybatisPlusConfig.java
@@ -1,9 +1,13 @@
 package com.azuray.decodeme.config;
 
+import javax.sql.DataSource;
+
 import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 
 @Configuration
@@ -11,29 +15,40 @@ import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 // MapperScannerConfigurer，2者配置1份即可
 public class MybatisPlusConfig {
 
-  /**
-   * mybatis-plus分页插件<br>
-   * 文档：http://mp.baomidou.com<br>
-   */
-  @Bean
-  public PaginationInterceptor paginationInterceptor() {
-    PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
-    paginationInterceptor.setDialectType("mysql");
-    return paginationInterceptor;
-  }
+    /**
+     * mybatis-plus分页插件<br>
+     * 文档：http://mp.baomidou.com<br>
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        paginationInterceptor.setDialectType("mysql");
+        return paginationInterceptor;
+    }
 
-  /**
-   * 相当于顶部的： {@code @MapperScan("com.baomidou.springboot.mapper*")} 这里可以扩展，比如使用配置文件来配置扫描Mapper的路径
-   */
-  @Bean
-  public MapperScannerConfigurer mapperScannerConfigurer() {
-    MapperScannerConfigurer scannerConfigurer = new MapperScannerConfigurer();
-    scannerConfigurer.setBasePackage("com.azuray.decodeme.mapper*");
-    return scannerConfigurer;
-  }
-//
-//  @Bean
-//  public H2KeyGenerator getH2KeyGenerator() {
-//    return new H2KeyGenerator();
-//  }
+    /**
+     * 相当于顶部的： {@code @MapperScan("com.baomidou.springboot.mapper*")} 这里可以扩展，比如使用配置文件来配置扫描Mapper的路径
+     */
+    @Bean
+    public MapperScannerConfigurer mapperScannerConfigurer() {
+        MapperScannerConfigurer scannerConfigurer = new MapperScannerConfigurer();
+        scannerConfigurer.setBasePackage("com.azuray.decodeme.mapper*");
+        return scannerConfigurer;
+    }
+
+    @Bean(name = "dataSource")
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setUrl(":3306/test?useUnicode=true&characterEncoding=UTF-8&useSSL=false&autoReconnect=true&failOverReadOnly=false&validationQuery=select 1");
+        dataSource.setUsername("");
+        dataSource.setPassword("");
+        dataSource.setDbType("mysql");
+        return dataSource;
+    }
+    //
+    //  @Bean
+    //  public H2KeyGenerator getH2KeyGenerator() {
+    //    return new H2KeyGenerator();
+    //  }
 }
